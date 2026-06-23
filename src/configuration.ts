47305aec6a37ee14f77a8ac7ffe24bf91a08d132
amwjmh <spq133@126.com>
2026-06-23 22:05:42 +0800

    refactor(config): 调整配置文件路径并新增环境配置
    1. 将原assets目录的配置文件迁移到src/config目录
    2. 新增开发、生产环境的yaml配置文件
    3. 移除冗余的redis.config.ts配置类
    4. 调整nest-cli.json的资源加载路径
    5. 新增基础bootstrap.yaml全局配置文件

--- src/configuration.ts
+++ src/configuration.ts
@@ -8,11 +8,12 @@ const YAML_CONFIG_SUFFIX = "yaml";
 
 export default () => {
   let config: Record<string, any> = null;
-  const configPath = path.resolve(__dirname, "assets", `${YAML_CONFIG_FILENAME}.${YAML_CONFIG_SUFFIX}`);
+  const configPath = path.resolve(__dirname, "config", `${YAML_CONFIG_FILENAME}.${YAML_CONFIG_SUFFIX}`);
+  console.log(configPath);
   if (existsSync(configPath)) {
     config = yaml.load(readFileSync(configPath, "utf-8"));
   }
-  const evnConfigPath = path.resolve(__dirname, "assets", `${YAML_CONFIG_FILENAME}-${process.env.NODE_ENV}.${YAML_CONFIG_SUFFIX}`);
+  const evnConfigPath = path.resolve(__dirname, "config", `${YAML_CONFIG_FILENAME}-${process.env.NODE_ENV}.${YAML_CONFIG_SUFFIX}`);
   if (existsSync(evnConfigPath)) {
     merge(config, yaml.load(readFileSync(evnConfigPath, "utf-8")));
   }
